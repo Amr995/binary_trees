63cc1fa55f83829354bcf48e50d5b8716474f0be
Amr995 <amr995@yahoo.com>
2024-06-06 04:36:58 -0700

    3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - free an entire tree using recursion it free the node
+ * when the node left and right are NULL
+ * @tree: tree to free
+ * Return: Nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t = *deleted_node;
+
+	deleted_node = tree;
+
+	if (deleted_node->parent == NULL)
+		return;
+	else
+		deleted_node->parent == NULL;
+}
